5cc49de78c794f86319ba2156fba03e8e2aea788
diana pava <dianapava65@gmail.com>
2023-07-15 16:40:41 -0500

    Se creo el tableView y la celda

---
+++ CinemaApp/CinemaApp/Home/MovieTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieTableViewCell.swift
+//  CinemaApp
+//
+//  Created by Diana Pava Avila on 15/07/23.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
--- CinemaApp/CinemaApp/ViewController.swift
+++ CinemaApp/CinemaApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let idMovieCell = "movicell"
+    var movies: [Int] = [1,2,3]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
+        
+
     }
-
-
+    
+    func setupTableView(){
+        let nib = UINib(nibName: "MovieTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: idMovieCell)
+        tableView.dataSource = self
+    }
 }
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: idMovieCell, for: indexPath) as? MovieTableViewCell else{
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    
+}
